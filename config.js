4247cae14a095774f8df43a54e3acc65538650a9
Tharusha-2008-maker <tldcreations2008@gmail.com>
2026-05-16 16:14:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Tharusha-2008-maker/KING-THARU-/blob/main/images/Gemini_Generated_Image_ri8lk3ri8lk3ri8l.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*_Hello👋 THARU-MD Is Alive Now😍_*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
